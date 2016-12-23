0509902f1e591dc8795f692dcb8c93bf427d87f9
Yexur <shimojax@gmail.com>
2016-12-22 21:46:48 -0600

    started adding angular
    added basic components .js files added ui-grid

---
+++ FitnessApp/src/FitnessApp/App/FitnessClass/fitnessClassController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('fitnessClassController', fitnessClassController);
+
+    fitnessClassController.$inject = ['$scope']; 
+
+    function fitnessClassController($scope) {
+        $scope.title = 'fitnessClassController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
--- FitnessApp/src/FitnessApp/wwwroot/_references.js
+++ FitnessApp/src/FitnessApp/wwwroot/_references.js
@@ -1,4 +1,7 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../app/app.js" />
+/// <reference path="../app/dataservice.js" />
+/// <reference path="../app/fitnessclass/fitnessclasscontroller.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
